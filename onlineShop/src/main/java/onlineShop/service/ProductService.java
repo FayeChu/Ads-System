ec46560de79ca46f0eaa76edeaa95cb3a322e7ab
Faye <chuxiaofei0725@gmail.com>
2018-09-10 15:16:42 -0700

    Implemented product controller

--- onlineShop/src/main/java/onlineShop/service/ProductService.java
+++ onlineShop/src/main/java/onlineShop/service/ProductService.java
@@ -1,13 +1,17 @@
-package onlineShop.dao;
+package onlineShop.service;
 
 import java.util.List;
 
 import onlineShop.model.Product;
 
-public interface ProductDao {
-	void addProduct(Product product);
-	void deleteProduct(int productId);
-	void updateProduct(Product product);
-	Product getProductById(int productId);
+public interface ProductService {
 	List<Product> getAllProducts();
+	
+	void addProduct(Product product);
+	
+	void deleteProduct(int productId);
+	
+	void updateProduct(Product product);
+	
+	Product getProductById(int productId);
 }
